48c9ec45176cb152d50f4145e0a7c2d8297b99a9
党凌轩 <2593651835@qq.com>
2022-07-11 13:05:05 +0800

    最终

--- springmvc08_01/src/main/java/com/op/ssm/handler/Handler01.java
+++ springmvc08_01/src/main/java/com/op/ssm/handler/Handler01.java
@@ -1,32 +1,47 @@
 package com.op.ssm.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @version : 1.0
- * @File : com.op.ssm.handler.Handler01
+ * @File : com.op.ssm.handler.Handler02
  * @Author : cjgong
- * @Time : 2022/6/19 14:36
+ * @Time : 2022/6/19 16:33
  * @desc :
  */
-// 把处理器注册到spring对象池里
 @Controller
-public class Handler01  {
+@Slf4j
+public class Handler01 {
+    /*
+    * 八大基本类型：
+    *
+    * */
+    @RequestMapping(value="/handler01/mehtod01",method = RequestMethod.GET)
+    public @ResponseBody String method01(int p1,boolean p2,@RequestParam("p4") double p3){
+        log.info(p1+"");
+        log.info(p2+"");
+        log.info(p3+"");
 
-    // 设置方法负责处理的请求
-    @RequestMapping(value="/handleRequest.action",method = RequestMethod.GET)
-    public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html;charset=utf-8");
-        PrintWriter out = response.getWriter();
-        out.write("hello springmvc");
+        return "succcess";
     }
+    @RequestMapping(value="/handler01/mehtod02",method = RequestMethod.GET)
+    public @ResponseBody String method02(Integer p1,boolean p2,@RequestParam(value = "p4",required = false) Double p3){
+        log.info(p1+"");
+        log.info(p2+"");
+        log.info(p3+"");
+        return "succcess";
+    }
+    //字符串类型
+    @RequestMapping(value="/handler01/mehtod03",method = RequestMethod.GET)
+    public @ResponseBody String method03(String p1,String p2){
+        log.info(p1+"");
+        log.info(p2+"");
+        return "succcess";
+    }
+
 }
